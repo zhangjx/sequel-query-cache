e7514631ceb51236f8e770043da7d89d7b435142
Joshua Hansen <joshua@amicus-tech.com>
2013-09-06 13:28:45 -0700

    Version bump to 0.2.1.

--- lib/sequel-query-cache/version.rb
+++ lib/sequel-query-cache/version.rb
@@ -4,7 +4,7 @@ module Sequel
     module QueryCache
       MAJOR_VERSION = 0
       MINOR_VERSION = 2
-      TINY_VERSION  = 0
+      TINY_VERSION  = 1
       VERSION = [MAJOR_VERSION, MINOR_VERSION, TINY_VERSION].join('.')
 
       def self.version; VERSION; end
